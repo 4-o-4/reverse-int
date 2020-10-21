5da7e2247d848621fb8acfb03aab61d07b697e34
4-o-4 <yuzhakov.lev@gmail.com>
2020-10-21 13:20:06 +0400

    feat: add reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n *= -1;
+    }
+
+    n = String(n);
+    n = n.split("").reverse().join("");
+    n *= 1;
+    return n;
+};
